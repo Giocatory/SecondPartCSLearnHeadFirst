7a82dc75309d5b0871a6fff038b2ec018dee4ce5
Giocatory <nivolyas@gmail.com>
2023-03-16 17:30:58 +0300

    16.03.23

--- HiveManagementSystem/logic/HoneyVault.cs
+++ HiveManagementSystem/logic/HoneyVault.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace HiveManagementSystem.logic
 {
-    class HoneyVault
+    static class HoneyVault
     {
+        public const float NECTAR_CONVERSION_RATIO = .19f;
+        public const float LOW_LEVEL_WARNING = 10f;
+
+        private static float honey = 25f;
+        private static float nectar = 100f;
     }
 }
