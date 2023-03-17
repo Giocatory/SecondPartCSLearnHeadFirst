90fff93586b70ae453023f185914bf445e4269c4
Giocatory <nivolyas@gmail.com>
2023-03-17 05:45:27 +0300

    17.03.23

--- HiveManagementSystem/logic/HoneyVault.cs
+++ HiveManagementSystem/logic/HoneyVault.cs
@@ -13,5 +13,55 @@ namespace HiveManagementSystem.logic
 
         private static float honey = 25f;
         private static float nectar = 100f;
+
+        public static string StatusReport
+        {
+            get 
+            {
+                if (honey < LOW_LEVEL_WARNING)
+                {
+                    return "LOW HONEY — ADD A HONEY MANUFACTURER";
+                }
+                else if (nectar < LOW_LEVEL_WARNING) { }
+                {
+                    return "LOW NECTAR — ADD A NECTAR MANUFACTURER";
+                }
+            }
+        }
+
+        public static void ConvertNectarToHoney(float amount)
+        {
+            if (nectar == 0)
+            {
+                return;
+            }
+            if (amount >= nectar) 
+            {
+                nectar = 0;
+            }
+            else
+            {
+                nectar -= amount;
+            }
+            honey += amount * NECTAR_CONVERSION_RATIO;
+        }
+
+        public static bool ConsumeHoney(float amount)
+        {
+            if (amount < honey)
+            {
+                amount -= honey;
+                return true;
+            }
+            return false;
+        }
+
+        public static void CollectNectar(float amount)
+        {
+            if(amount > 0)
+            {
+                honey += amount;
+            }
+        }
     }
 }
